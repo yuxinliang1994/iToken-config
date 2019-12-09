3a6765074bf96d892c67fc3cdc306dc7d000c28b
yuxl <164553349@qq.com>
2019-12-09 19:09:45 +0800

    初始化

--- src/main/java/com.itoken/ConfigApplication.java
+++ src/main/java/com.itoken/ConfigApplication.java
@@ -3,13 +3,11 @@ package com.itoken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import javax.security.auth.login.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer //配置中心服务端
-@EnableEurekaClient //开启Eureka服务提供者
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
