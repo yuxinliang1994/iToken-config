80246659d429d0f1de5e299a091bcb876cb61c6f
yuxl <164553349@qq.com>
2019-12-12 13:16:48 +0800

    初始化

--- src/main/java/com/itoken/config/ConfigApplication.java
+++ src/main/java/com/itoken/config/ConfigApplication.java
@@ -1,10 +1,8 @@
-package com.itoken;
+package com.itoken.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-import javax.security.auth.login.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer //配置中心服务端
